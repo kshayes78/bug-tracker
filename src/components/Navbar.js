4c99566b29ad15cac8c5a8d8d95565968ad0ac3f
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-06-07 16:28:23 -0400

    working on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+//styles & images
+import './Navbar.css';
+import Bug from '../assets/bug.svg';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <ul>
+        <li className='logo'>
+          <img src={Bug} alt='dojo logo' />
+          <span>Bug Tracker</span>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/signup'>Signup</Link>
+        </li>
+        <li>
+          <button className='btn'>Logout</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
